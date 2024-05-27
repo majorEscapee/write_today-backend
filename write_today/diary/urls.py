b02abd7ee3d517054d9c6b4bd773676f9a38db3a
egaeng <egaeng09@gmail.com>
2024-05-27 20:11:30 +0900

    0527

--- write_today/diary/urls.py
+++ write_today/diary/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('friend/accept/', views.AcceptFriend.as_view()),
 
     path('diarys', views.DiaryList.as_view()),
-    path('diary', views.DiaryDetail.as_view()),
+    path('diary/', views.DiaryDetail.as_view()),
     path('diary/<pk>', views.DiaryDetailPk.as_view()),
     path('write', views.WriteDiary.as_view()),
     path('result/<pk>', views.ResultDetail.as_view()),
